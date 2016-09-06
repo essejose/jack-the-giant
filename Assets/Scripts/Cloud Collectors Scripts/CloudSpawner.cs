6ef7a1993a78d28e69d934261865f928c80399fe
essejose <ze_pk2@hotmail.com>
2016-09-05 23:27:29 -0300

    trabalhando no spwan de nuvens

---
+++ Assets/Scripts/Cloud Collectors Scripts/CloudSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] clouds;
+
+    private float distanceBetweenClouds = 3f;
+
+    private float minX, maxX;
+
+    private float controlX;
+
+    private float lastCloudPositionY;
+
+    [SerializeField]
+    private GameObject[] collectables;
+
+    private GameObject player;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
