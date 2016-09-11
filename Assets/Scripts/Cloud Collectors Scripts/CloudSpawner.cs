b4a09eea3d770ecb60bcf424fa6a903a12425051
essejose <ze_pk2@hotmail.com>
2016-09-11 18:57:44 -0300

    criando nuvens randomicas em zig zag

--- Assets/Scripts/Cloud Collectors Scripts/CloudSpawner.cs
+++ Assets/Scripts/Cloud Collectors Scripts/CloudSpawner.cs
@@ -20,13 +20,73 @@ public class CloudSpawner : MonoBehaviour {
     private GameObject player;
 
 
+
     // Use this for initialization
-    void Start () {
+    void Awake () {
+        controlX = 0;
+        SetMininAndMax();
+        CreateClounds();
+
+    }
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //Criando minino e maximo dentro do game view
+	void SetMininAndMax(){
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+
+        maxX = bounds.x - 0.5f;
+        minX = -bounds.x + 0.5f;
+    }
+
+    void Shuffle(GameObject[] ArrayToShuffle){
+        for(int i = 0; i < ArrayToShuffle.Length; i++){
+            GameObject temp = ArrayToShuffle[i];
+            int random = Random.Range(i, ArrayToShuffle.Length);
+            ArrayToShuffle[i] = ArrayToShuffle[random];
+            ArrayToShuffle[random] = temp;
+
+
+        }
+
+    }
+    void CreateClounds(){
+
+        Shuffle(clouds);
+
+        float positionY = 0f;
+    
+        for(int i = 0; i < clouds.Length; i++){
+
+            Vector3 temp = clouds[i].transform.position;
+
+            temp.y = positionY;
+
+            //temp.x = Random.Range(minX, maxX);
+
+            if(controlX == 0) {
+                temp.x = Random.Range(0.0f, maxX);
+                controlX = 1;
+            }else if(controlX == 1){
+                temp.x = Random.Range(0.0f, minX);
+                controlX = 2;
+            }
+            else if (controlX == 2)
+            {
+                temp.x = Random.Range(1.0f, maxX);
+                controlX = 3;
+            }
+            else if (controlX == 3)
+            {
+                temp.x = Random.Range(-1.0f, minX);
+                controlX = 0;
+            }
+            lastCloudPositionY = positionY;
+
+            clouds[i].transform.position = temp;
+            positionY -= distanceBetweenClouds;
+
+
+
+
+        }
+    }
 }
