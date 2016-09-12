23888d8bf5a14871e90d7e051495beb83e315b2c
essejose <ze_pk2@hotmail.com>
2016-09-12 00:23:30 -0300

    posicionando o player

--- Assets/Scripts/Cloud Collectors Scripts/CloudSpawner.cs
+++ Assets/Scripts/Cloud Collectors Scripts/CloudSpawner.cs
@@ -26,7 +26,14 @@ public class CloudSpawner : MonoBehaviour {
         controlX = 0;
         SetMininAndMax();
         CreateClounds();
+        player = GameObject.Find("Player");
+     
 
+    }
+
+    void Start()
+    {
+        PositionThePlayer();
     }
 	
     //Criando minino e maximo dentro do game view
@@ -89,4 +96,34 @@ public class CloudSpawner : MonoBehaviour {
 
         }
     }
+
+    void PositionThePlayer(){
+        GameObject[] darkClouds = GameObject.FindGameObjectsWithTag("Deadly");
+        GameObject[] cloudsInGame = GameObject.FindGameObjectsWithTag("Cloud");
+
+        for (int i = 0; i < darkClouds.Length; i++)
+        {
+
+            if (darkClouds[i].transform.position.y == 0f)
+            {
+
+                Vector3 t = darkClouds[i].transform.position;
+                darkClouds[i].transform.position = new Vector3(cloudsInGame[0].transform.position.x,
+                                                                cloudsInGame[0].transform.position.y,
+                                                                cloudsInGame[0].transform.position.z);
+                cloudsInGame[0].transform.position = t;
+            }
+        }
+        Vector3 temp = cloudsInGame[0].transform.position;
+
+        for(int i = 0; i < cloudsInGame.Length; i++){
+            if(temp.y < cloudsInGame[i].transform.position.y){
+                temp = cloudsInGame[i].transform.position;
+            }
+            }
+
+        temp.y += 0.8f;
+        player.transform.position = temp;
+        }
 }
+ 
