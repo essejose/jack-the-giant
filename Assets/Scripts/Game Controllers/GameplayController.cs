cd4e206a03a34e3b747650a09664a9c247f8c73e
essejose <ze_pk2@hotmail.com>
2016-09-23 23:28:45 -0300

    create a Game Manage, coin collectable done fixed #3

---
+++ Assets/Scripts/Game Controllers/GameplayController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameplayController : MonoBehaviour {
+
+
+    public static GameplayController instance;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void MakeInstance()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+}
